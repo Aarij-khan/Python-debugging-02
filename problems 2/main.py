a00063e0b6d991042e4aca594baf33bca73609d1
Aarij khan <aarij0100@gmail.com>
2025-01-17 20:37:59 +0500

    Add files via upload

---
+++ problems 2/main.py
@@ -0,0 +1,53 @@
+# import time
+
+# num = 1
+# if(num > 0):
+#     print(num,"is positive")
+# elif(num < 0):    
+#     print(num,"is negative")
+# elif(num == 0):    
+#     print(num,"its zero")
+# else:
+#     print("enter number")  
+  
+
+# year = time.strftime("%Y")
+# yearNum = int(year)
+# if (yearNum % 4 == 0 and yearNum % 100 != 0) or (yearNum % 400 == 0):
+#     print(f"{yearNum} is a leap year.")
+# else:
+#     print(f"{yearNum} is not a leap year.")
+
+
+# val = input("Enter any character")
+# if(val == "a") or (val == "e") or (val == "i") or (val == "o") or (val == "u"):
+#     print(f"{val} is vowel")
+# else:
+#     print(f"{val} is not a vowel")
+
+
+
+# num = int(input("Enter your grade"))
+# if (num >= 90) :
+#     print("Your grade is A")
+# elif (num > 80 and num < 89):    
+#     print("Your grade is B")
+# elif (num > 70 and num < 79):    
+#     print("Your grade is C")
+# elif (num > 60 and num < 69):    
+#     print("Your grade is D")
+# else:
+#     print("You are fail") 
+
+# val1 = int(input("Enter first numbers"))
+# val2 = int(input("Enter second numbers"))
+# val3 = int(input("Enter three numbers"))
+
+# if val1>val2 and val1>val3:
+#   print(f"{val1} is the largest.")
+# elif val2>val1 and val2>val3:
+#   print(f"{val2} is the largest.")
+# else:
+#   print(f"{val3} is the largest.")
+    
+
